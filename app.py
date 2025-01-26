d2b61cd009182e6d7fcf731d78c1611f0d8e5fc0
RileyGonz <rileygonz1216@gmail.com>
2025-01-25 18:37:21 -0600

    added datebase

--- app.py
+++ app.py
@@ -1,1 +1,65 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finances.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    initial_amount = db.Column(db.Float, nullable=False)
+    hourly_wage = db.Column(db.Float, nullable=False)
+    hours_per_week = db.Column(db.Float, nullable=False)
+
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+
+class Bonus(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+
+
+class NonWorkingDay(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+
+
+class PayException(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+
+
+@app.route('/expenses', methods=['POST'])
+def add_expense():
+    data = request.get_json()
+    new_expense = Expense(
+        user_id=data['user_id'],
+        description=data['description'],
+        amount=data['amount'],
+        date=datetime.strptime(data['date'], '%Y-%m-%d')
+    )
+    db.session.add(new_expense)
+    db.session.commit()
+    return jsonify({'id': new_expense.id}), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
